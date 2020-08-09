e655296d2c335e8b66efc7a97a795bb38f75133e
MasterS007 <munirasirajum13@gmail.com>
2020-08-09 10:38:27 +0600

    reg, login added

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+	<form action="logcheck.php" method="POST">
+		<fieldset style="width:max-content;">
+			<legend>LOGIN</legend>
+		<table>
+			<tr>
+				<td>User Id :</td>
+				<td><input type="text" name="id"></td>
+			</tr>
+			<tr>
+				<td>Password :</td>
+                <td><input type="password" name="password"></td>
+               
+            </tr>
+           
+		</table>
+           
+        <hr>
+              <input type="checkbox" name="checkRemember">Remember Me
+               <br><br>
+              <input type="submit" name="submit" value="Login"> <a href="registration.html">Register</a>
+			
+	
+		</fieldset>
+	</form>
+</body>
+</html>
+    
+	
+
