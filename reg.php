6b1762b58e02041108105da5e68385b628b20de3
MasterS007 <munirasirajum13@gmail.com>
2020-08-09 10:54:39 +0600

    db added to reg

--- reg.php
+++ reg.php
@@ -1,8 +1,8 @@
 <?php
  
- session_start();
  if(isset($_POST['submit']))
- {
+ {  
+        $conn = mysqli_connect('localhost','root','','webtech_mid_project','3306');
 
         $id = $_POST['id'];
         $name = $_POST['name'];
@@ -10,14 +10,16 @@
         $password = $_POST['password'];
         $conpassword = $_POST['confirmPassword'];
         $user = $_POST['user'];
-        
+        $valid = FALSE;
         
         if(empty($name)||empty($email)||empty($password))
         {
+          $valid = FALSE;
             echo "null submission";
         }
         else if(empty($conpassword)||!isset($_POST['user']))
         {
+          $valid = FALSE;
           echo "null submission";
 
         }
@@ -25,36 +27,51 @@
         
         elseif($password != $conpassword)
         {
+          $valid = FALSE;
           echo "Warrning: Password and Confirm Password are not matched!";
           echo "Please, do registration again!";
         }
        
 
         else {
+      
+          
+              $valid =TRUE;   
+            }
 
-          if($_POST['gender']=='Male')
-          {
-            $gender = 'Male';
-               
-          }
-          elseif($_POST['gender']=='Female')
-          {
-            $gender = 'Female';
-          }
+        if($valid==TRUE)
+        {
+          
+          $sql = "INSERT INTO users (id, uName, uPassword, uEmail, uType) VALUES ('$id','$name','$password','$email','$user')";
 
-          elseif($_POST['gender']=='Other'){
-            $gender = 'Other';
+          if(mysqli_query($conn, $sql))
+          {
+             
+              header('location:login.php');
+              echo "Insert successfull";
+             
           }
+             
+          else
+          {
+           
+           // $svalid =FALSE;
+           echo "Insert unsuccessfull";
+           echo '<br>'.mysqli_errno($conn).'<br>';
+           print_r(mysqli_error_list($conn));
+            
+          }
+          mysqli_close($conn);
           
          
-              $file = fopen('user.txt', 'a');
-			        fwrite($file,$id.'|'.$user.'|'. $name.'|'.$password.'|'.$email."/r/n");
-              fclose($file);
-              $_SESSION['info']="ok";
-    
-        
-          header('location:login.php');
+       }
+
+        else
+        {
+         
+          header('location:registration.html');
         }
+      
     
       }
       else{
